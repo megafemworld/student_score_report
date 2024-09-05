c3f705e3cd18c350e097fe087919dfae58632469
Femi Mehalayese <megafemclick@gmail.com>
2024-09-05 06:21:14 +0100

    update

--- web_app/form.py
+++ web_app/form.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, RadioField, SubmitField, IntergerField, SubmitField
 from wtforms.validators import DataRequired
-from werkzeug.security import generate_password_hash,  check_password_hash
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
