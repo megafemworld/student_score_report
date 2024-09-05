9dbd98471b5112cb35ce3f920afe361b4ac310f6
Femi Mehalayese <megafemclick@gmail.com>
2024-09-05 19:33:14 +0100

    major

--- web_app/auth.py
+++ web_app/auth.py
@@ -1,4 +1,9 @@
-from flask import Flask, request, flash, render_template
+from flask import Flask, redirect, request, flash, render_template, url_for
+"""
+Renders the teacher page with assigned subjects categorized by class.
+Returns:
+    rendered template: The HTML template for the teacher page.
+"""
 from flask_login import LoginManager, UserMixin, login_required, current_user
 from functools import wraps
 from . import form
@@ -15,6 +20,13 @@ users = {
     'student_user': {'id': '3', 'username': 'st_student_user', 'password': 'password'}
 }
 
+
+assigned_subjects = [
+    {'id': '1', 'teacher_id': '1', 'subject_name': 'Mathematics', 'class_name': 'JSS1', 'term_name': '1', 'year_name': '2024/2025'},
+    {'id': '2', 'teacher_id': '1', 'subject_name': 'Mathematics', 'class_name': 'JSS2', 'term_name': '2', 'year_name': '2024/2025'},
+    {'id': '3', 'teacher_id': '1', 'subject_name': 'Mathematics', 'class_name': 'JSS3', 'term_name': '3', 'year_name': '2024/2025'},
+    {'id': '4', 'teacher_id': '1', 'subject_name': 'Physics', 'class_name': 'JSS2', 'term_name': '2', 'year_name': '2024/2025'}
+]
 
 def get_user_role(username):
     if username.startswith('adm'):
@@ -49,6 +61,14 @@ def role_required(required_role):
             return f(*args, **kwargs)
         return decorated_function
     return decorator
+
+
+def check_assign(class_name, subject, year, term):
+    for assigned_subject in assigned_subjects:
+        if assigned_subject['class_name'] == class_name and assigned_subject['subject_name'] == subject and assigned_subject['year_name'] == year and assigned_subject['term_name'] == term and assigned_subject['teacher_id'] == current_user.id:
+            pass
+        else:
+            flash('Subject not assigned to you.', 'danger')
 
 @app.route('/add/who', methods=['GET', 'POST'])
 @login_required
@@ -98,5 +118,34 @@ def login():
 @login_required
 @role_required('teacher')
 def teacher():
-    return render_template('teacher.html')
+    class_subjects = {}
+    for subject in assigned_subjects:
+        class_name = subject['class_name']
+        subject_name = subject['subject_name']
+        if class_name in class_subjects:
+            class_subjects[class_name].append(subject_name)
+        else:
+            class_subjects[class_name] = [subject_name]
+    
+    return render_template('teacher.html', class_subjects=class_subjects)
 
+@app.route('/teacher/result_upload/<class_name>/<subject>/<year:int>/<term:int>', methods=['GET', 'POST'])
+@login_required
+@role_required('teacher')
+def teacher_upload():
+    class_name = request.view_args['class_name']
+    subject = request.view_args['subject']
+    year = int(request.view_args['year'])
+    term = int(request.view_args['term'])
+    if request.method == 'POST' or request.method == 'GET':
+        check_assign(class_name, subject, year, term)
+        #form data
+        students = [
+    {'id': '1', 'username': 'st_student_user', 'first_name': 'Faith', 'last_name': 'Chike', 'password': 'password', 'class_name': 'JSS1', 'year_name': '2024/2025'},
+    {'id': '2', 'username': 'st_student_user', 'first_name': 'Hajarat', 'last_name': 'Bello', 'password': 'password', 'class_name': 'JSS2', 'year_name': '2024/2025'},
+    {'id': '3', 'username': 'st_student_user', 'first_name': 'Dayo', 'last_name': 'Bola', 'password': 'password', 'class_name': 'JSS3', 'year_name': '2024/2025'}
+    {'id': '4', 'username': 'st_student_user', 'first_name': 'Ibrahim', 'last_name': 'Buhari', 'password': 'password', 'class_name': 'JSS1', 'year_name': '2024/2025'}
+]
+        if form.validate_on_submit():
+            pass
+        return render_template('result_upload.html', form=form, students=students)
