77b2701e5d0d806999b5b9f378c33126456db10c
Femi Mehalayese <megafemclick@gmail.com>
2024-09-23 18:50:11 +0100

    sql file changed

--- web_app/db_record/database.sql
+++ web_app/db_record/database.sql
@@ -1,6 +1,6 @@
 mysql -u root -p
 CREATE DATABASE IF NOT EXISTS `schooldb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER 'don'@'localhost' IDENTIFIED BY '08033191820Ee';
+CREATE USER 'eben'@'localhost' IDENTIFIED BY '0267419026*Ee';
 GRANT ALL PRIVILEGES ON `schooldb`.* TO 'eben'@'localhost';
 FLUSH PRIVILEGES;
 
